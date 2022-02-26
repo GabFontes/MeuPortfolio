2875b8b188f7e7c967da2591b2aa469ca3cf19f6
gab <gabrielfontes321@gmail.com>
2022-02-26 01:03:23 -0300

    Initial Setup. Installing libraries.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        aquamarine: "#14b297",
+        neutral: "#0f0f0f",
+        semineutral: '#121212'
+      },
+    },
+  },
+  plugins: [],
+}
