12b6b37b914b4e25a24f35f3c0f44bf2c08f67de
gab <gabrielfontes321@gmail.com>
2022-02-26 01:07:44 -0300

    feat: Implementing Knowledges screen and title

---
+++ src/components/Knowledges/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Title, Knowledge} from './style'
+
+export function Knowledges() {
+  return (
+    <Knowledge>
+      <Title>Conhecimentos e Tecnologias</Title>
+    </Knowledge>
+  )
+}
+
+export default Knowledges;
---
+++ src/components/Knowledges/style.js
@@ -0,0 +1,16 @@
+import tw from "tailwind-styled-components";
+
+export const Title = tw.h1`
+text-white
+text-4xl
+self-center
+`
+
+
+export const Knowledge = tw.h1`
+text-white
+bg-semineutral
+h-full
+flex
+justify-center
+`
